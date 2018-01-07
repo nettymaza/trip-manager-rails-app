bcf9d5990aac99fc744bd2c6a4390614f3ff9491
Annette Maza <nettymaza@MacBook-Pro.local>
2018-01-07 15:03:37 -0700

    starting rails scaffolding

---
+++ config/routes.rb
@@ -0,0 +1,23 @@
+Rails.application.routes.draw do
+
+  # Root 'home' controller
+  root 'welcome#home'
+
+  # # Users controller
+  # get  '/signup',  to: 'users#new'
+  # post '/signup',  to: 'users#create'
+  # resources :users, :only => [:new, :create, :show]
+  #
+  # # Sessions Controller
+  # get '/login' => 'sessions#new'
+  # post '/login' => 'sessions#create'
+  # delete '/logout',  to: 'sessions#destroy'
+  #
+  #
+  # #Sessios Omniauthable
+  # get '/auth/:provider/callback', to: 'sessions#create'
+  #
+  # #Trip routes
+  # resources :trips
+
+end
